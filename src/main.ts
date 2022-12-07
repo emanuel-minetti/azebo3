b247b08820f5c4946ce14860bb11a0a28dd235f2
Emanuel Minetti <dvorg22@intra.udk-berlin.de>
2022-12-07 15:36:33 +0100

    Customized bootstrap.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./scss/custom.scss";
 import "bootstrap";
 
 const app = createApp(App);
