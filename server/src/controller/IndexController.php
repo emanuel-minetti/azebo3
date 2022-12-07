5654f28d47b95b419bd4629e205de08500f0cbfb
Emanuel Minetti <dvorg22@intra.udk-berlin.de>
2022-12-07 17:14:20 +0100

    Started using a DB.

--- server/src/controller/IndexController.php
+++ server/src/controller/IndexController.php
@@ -1,8 +1,18 @@
 <?php
 namespace controller;
+use PDO;
+
 class IndexController {
+    private $user = 'azebo3';
+    private $pw = 'cGd*b3x8kpkNLkqo';
     public function indexAction(): void {
         $url = $_REQUEST['url'];
-        echo "Hallo von der API! Die URL war $url";
+        echo "Hallo von der API! Die URL war '$url'";
+
+        $dbh = new PDO('mysql:host=localhost;dbname=azebo3', $this->user, $this->pw);
+        $stm = $dbh->query('SELECT name FROM user WHERE id = 1');
+        $res = $stm->fetch(PDO::FETCH_ASSOC);
+        $name = $res['name'];
+        echo "<br/>Dein Name ist: '$name'";
     }
 }
