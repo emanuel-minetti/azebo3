b5d36a21016f56a261010c5cd45dc2d174696f0f
Emanuel Minetti <e.minetti@posteo.de>
2022-12-20 19:53:30 +0100

    Added client side validation and a fake action to the AuthStore.

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,9 +1,25 @@
 import { defineStore } from "pinia";
+import router from "@/router";
 
+// noinspection JSUnusedLocalSymbols
 export const useAuthStore = defineStore({
   id: "auth",
   state: () => ({
     user: JSON.parse(sessionStorage.getItem("user") ?? "null"),
-    returnURL: null,
+    redirectURL: null,
   }),
+  actions: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    login(username: string, password: string) {
+      const user = {
+        id: 1,
+        name: "Hallo Du",
+        validTo: new Date(new Date().getTime() + 30 * 60000),
+      };
+      this.user = user;
+      sessionStorage.setItem("user", JSON.stringify(user));
+      // noinspection JSIgnoredPromiseFromCall
+      router.push(this.redirectURL || "/");
+    },
+  },
 });
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-import { ref } from "vue";
-
-const username = ref("");
-const password = ref("");
-
-function submit(evt: Event) {
-  evt.preventDefault();
-  console.log("Submitted");
-  console.log(username.value);
-}
-</script>
-
 <template>
   <h1>Login</h1>
   <form @submit="(evt) => submit(evt)">
@@ -22,6 +9,9 @@ function submit(evt: Event) {
         id="username"
         v-model="username"
       />
+      <div class="alert alert-warning" v-show="usernameErrorPresent">
+        Der Benutzername muss zwischen 3 und 30 Zeichen lang sein.
+      </div>
     </div>
     <div class="mb-3">
       <label for="password" class="form-label">Passwort</label>
@@ -31,7 +21,49 @@ function submit(evt: Event) {
         id="password"
         v-model="password"
       />
+      <div class="alert alert-warning" v-show="passwordErrorPresent">
+        Das Passwort muss zwischen 8 und 30 Zeichen lang sein.
+      </div>
     </div>
     <button type="submit" class="btn btn-primary">Login</button>
   </form>
 </template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { useAuthStore } from "@/stores";
+
+const authStore = useAuthStore();
+const username = ref("");
+const password = ref("");
+const error = ref(
+  new Map([
+    ["username", false],
+    ["password", false],
+  ])
+);
+const usernameErrorPresent = computed(() => error.value.get("username"));
+const passwordErrorPresent = computed(() => error.value.get("password"));
+
+function submit(evt: Event) {
+  evt.preventDefault();
+  validate();
+  if (Array.from(error.value.values()).every((value) => !value)) {
+    authStore.login(username.value, password.value);
+  }
+}
+
+function validate() {
+  validateUsername(username.value);
+  validatePassword(password.value);
+  console.log(error.value);
+}
+
+function validateUsername(username: string) {
+  error.value.set("username", !(username.length >= 3 && username.length <= 30));
+}
+
+function validatePassword(password: string) {
+  error.value.set("password", !(password.length >= 8 && password.length <= 30));
+}
+</script>
